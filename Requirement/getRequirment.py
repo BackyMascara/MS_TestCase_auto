31a32ca20c466abc51f2540e4f90a0c3eaf4c862
keya <1262832227@qq.com>
2024-02-08 09:47:07 +0800

    Changes

--- Requirement/getRequirment.py
+++ Requirement/getRequirment.py
@@ -4,12 +4,15 @@ import requests
 import jsonpath
 from common.getKeyword_forResult import GetKeyword
 from common.getExcel import GetExcel
+from common.getExcel import GetExcels
 
-my_time = '20240204-20240223'
+my_time = '20240204-20240223' # *必填，迭代周期
 requirementKey = 'Q00001705'
+# *必填，需求文档编码
+requirementKeys = ('Q00002293', 'Q00002292','Q00002291','Q00002290','Q00002289')
 
 req_url = 'https://mgt.mysteelcms.com/dataspider-multi/api/spiderRequirement/requirementDetail/{}'.format(requirementKey)
-headers = {"Cookie":"JSESSIONID=CA6C111F48639C7C012A6493A8F4B064; JSESSIONID=A2F1ACA6EB5BF9DBC9FAEF22D58CCC05; _center_login_token=45d381375eb548199fceebc587c1877d; _login_token=45d381375eb548199fceebc587c1877d; _last_loginaid=c541c265c13f5644a79cc193f9a8065e; mysteel_sso_ticket=9a1809cc66984abab996ce364138c19e; buriedDistinctId=412baa16e5c34c799f42274224928fb3; BURIED_STROAGE.mysteelcms.com=eyJTRUVTSU9OSUQiOiI0MzBiMmMzMjM1ZmU1NTM2IiwiU0VFU0lPTkRBVEUiOjE3MDcwOTc5NDA2OTUsIkFOU0FQUElEIjoiIiwiQU5TJERFQlVHIjowLCJBTlNVUExPQURVUkwiOiIvL3N0YXRzLm15c3RlZWwuY29tIiwiRlJJU1REQVkiOiIyMDIzMDgwMSJ9"}
+headers = {"Cookie":"JSESSIONID=14BB3A171EF932E823AAEE115702D5C4; _login_token=d1311fd74fdf4efe936f545ca43fe8dc; _center_login_token=d1311fd74fdf4efe936f545ca43fe8dc; _last_loginaid=124e5e35196b0e0c3b47df1e1d30838f; mysteel_sso_ticket=3d5d75e9fe4e4ee5b36d0fd6d3b2de26; JSESSIONID=4DCCD47BF1201A99BD2E1C9F0559A799"}
 def getRequirment():
     res = requests.request(method='get',url=req_url,headers=headers)
     res = res.json()
@@ -24,5 +27,49 @@ def getRequirment():
     print(fieldNames)
     GetExcel(my_time,requirementName,tableName,fieldNames)
 
+# def getRequirments():
+#     requirementKeys = ('Q00002367','Q00002366')
+#     print(len(requirementKeys))
+#     for requirementKey in requirementKeys:
+#         req_url = 'https://mgt.mysteelcms.com/dataspider-multi/api/spiderRequirement/requirementDetail/{}'.format(requirementKey)
+#         res = requests.request(method='get', url=req_url, headers=headers)
+#         res = res.json()
+#
+#         requirementName = GetKeyword.get_keyword(res, 'requirementName')  # 需求名
+#         tableName = GetKeyword.get_keyword(res, 'tableName')  # 需求表名
+#         fieldList = res["response"]["fieldList"]  # 表信息对象
+#
+#         fieldNames = []  # 表字段名
+#         for field in fieldList:
+#             fieldNames.append(field["fieldName"])
+#         print(fieldNames)
+#         GetExcel(my_time, requirementName, tableName, fieldNames)
+
+def getRequirments():
+    requirementNames = []  # 所有需求名称
+    tableNames = []  # 所有表名
+    fieldNames = []  # 所有表字段
+
+    for requirementKey in requirementKeys:
+        req_url = 'https://mgt.mysteelcms.com/dataspider-multi/api/spiderRequirement/requirementDetail/{}'.format(requirementKey)
+        res = requests.request(method='get', url=req_url, headers=headers)
+        res = res.json()
+
+        requirementName = GetKeyword.get_keyword(res, 'requirementName')  # 需求名
+        tableName = GetKeyword.get_keyword(res, 'tableName')  # 需求表名
+        fieldList = res["response"]["fieldList"]  # 表信息对象
+
+        fieldName = []  # 表字段名
+        for field in fieldList:
+            fieldName.append(field["fieldName"])
+        fieldNames.append(fieldName)
+        requirementNames.append(requirementName)
+        tableNames.append(tableName)
+
+    print(fieldNames)
+    print(requirementNames)
+    print(tableNames)
+    GetExcels(my_time, requirementNames, tableNames, fieldNames)
+
 if __name__ == '__main__':
-    getRequirment()
+    getRequirments()
--- common/getExcel.py
+++ common/getExcel.py
@@ -1,9 +1,17 @@
 from openpyxl import load_workbook
 
 def GetExcel(my_time,requirementNames,tableNames,fieldNames):
-
+    """
+    用于对单个需求文档，编写Excel测试用例
+    :param my_time: 迭代周期时间
+    :param requirementNames: 需求数组
+    :param tableNames: 表名数组
+    :param fieldNames: 表字段数组
+    :return:
+    """
     # 打开现有的模板Excel文件
-    wb = load_workbook(r'E:\测试用例-fl\爬虫\20240204-20240223\测试用例-自动.xlsx')
+    #wb = load_workbook(r'E:\测试用例-fl\爬虫\20240204-20240223\测试用例-自动.xlsx')
+    wb = load_workbook(r'../data/测试用例-模板.xlsx')
 
     # 选择要操作的工作表（默认为第一个）
     ws = wb.active
@@ -36,9 +44,81 @@ def GetExcel(my_time,requirementNames,tableNames,fieldNames):
     wb.save(save_url)
     wb.close()
 
+def GetExcels(my_time,requirementNames,tableNames,fieldNames):
+    """
+    用于对多个需求文档，编写Excel测试用例
+    :param my_time: 迭代周期时间
+    :param requirementNames: 需求数组
+    :param tableNames: 表名数组
+    :param fieldNames: 表字段数组
+    :return:
+    """
+    # 打开现有的模板Excel文件
+    #wb = load_workbook(r'E:\测试用例-fl\爬虫\20240204-20240223\测试用例-自动.xlsx')
+    wb = load_workbook(r'../data/测试用例-模板.xlsx')
+
+    # 选择要操作的工作表（默认为第一个）
+    ws = wb.active
+    i = 2  # 测试用例名称的单元格位置
+    j = 2  # 测试步骤的单元格位置
+
+    index = 0 # 需求游标
+    # 遍历每一个需求
+    for requirementName in requirementNames:
+        A_index = 'A' + str(i)  # 用例名称
+        B_index = 'B' + str(i)  # 所属模块
+        C_index = 'C' + str(i)  # 标签
+        D_index = 'D' + str(j)  # 步骤描述
+        E_index = 'E' + str(j)  # 预期结果
+        F_index = 'F' + str(i)  # 编辑模式
+        G_index = 'G' + str(i)  # 用例状态
+        H_index = 'H' + str(i)  # 责任人
+        I_index = 'I' + str(i)  # 用例等级
+        J_index = 'J' + str(i)  # 用例分类
+
+
+        # 设置要写入的单元格位置及值
+        ws[A_index] = '检查“ ' + requirementName + ' ”需求数据'
+        ws[B_index] = '/' + my_time + '/' + requirementName + ' ( ' + tableNames[index] + ' )'
+        ws[C_index] = '数据'
+        ws[F_index] = 'STEP'
+        ws[G_index] = '未开始'
+        ws[H_index] = '樊磊'
+        ws[I_index] = 'P0'
+        ws[J_index] = '[后端]'
+
+        # 步骤：前两行固定写死（第一行）
+        ws[D_index] = '数据准确性'
+        ws[E_index] = '与原网站数据一致'
+
+        j = j + 1  # 第二行
+        D_index = 'D' + str(j)  # 步骤描述
+        E_index = 'E' + str(j)  # 预期结果
+        ws[D_index] = '与原网站数据一致'
+        ws[E_index] = '与原网站数据一致'
+
+        j = j + 1  # 第三行开始，遍历每个需求的表格字段
+        for fieldName in fieldNames[index]:
+
+            D_index = 'D' + str(j)  # 步骤描述
+            E_index = 'E' + str(j)  # 预期结果
+            ws[D_index] = '检查mysteel_spider.' + tableNames[index] + ' 表 ' + fieldName + ' 字段数据'
+            ws[E_index] = '符合前置需求文档字段清洗逻辑和质检逻辑'
+
+            j = j + 1
+
+        index = index + 1
+        i = index * 23 + 2  # 下一个合并单元格（下一个需求）
+        j = index * 23 + 2
+
+    # 保存修改并关闭Excel文件
+    save_url = r'E:\测试用例-fl\爬虫\{}\测试用例-{}.xlsx'.format(my_time, "test")
+    wb.save(save_url)
+    wb.close()
+
 if __name__ == '__main__':
     my_time = '20240204-20240223'
-    fieldNames = ['data_date', 'breed_name', 'maturity_num', 'maturity_num_1m', 'maturity_num_2m', 'remark']
-    requirementNames = '上海期货交易所标准仓单到期数量'
-    tableNames = 'fu_spi_wh_maturity_num'
-    GetExcel(my_time,requirementNames,tableNames,fieldNames)
+    fieldNames = [['data_date', 'breed_name', 'maturity_num', 'maturity_num_1m', 'maturity_num_2m', 'remark'],['data_date', 'breed_name', 'maturity_num', 'maturity_num_1m', 'maturity_num_2m', 'remark0207']]
+    requirementNames = ['深圳证券交易所','上海证券交易所']
+    tableNames = ['eco_spi_power_mddl','eco_spi_capacity_ggdl']
+    GetExcels(my_time,requirementNames,tableNames,fieldNames)
